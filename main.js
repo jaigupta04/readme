98f3bbc7edcbc63ceab37e99834c63a892b9fb54
Jai Gupta <jaiwork04@gmail.com>
2024-06-17 16:11:04 +0530

    vercel

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 (async () => {
+  process.env.PORT  = process.env.PORT  || 8080;
   
   require('./app').listen(process.env.PORT, () => {
     console.log(`Server is up and listening at ${process.env.PORT}`)
